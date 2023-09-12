51050df83bf1ec60a8935f14de9543796f864a5f
mChramer <106435380+mChramer@users.noreply.github.com>
2023-09-12 16:22:07 +0200

    Made an anonymous function

--- App/Program.cs
+++ App/Program.cs
@@ -9,28 +9,27 @@ namespace nsMath
         {
             Timer timer = new Timer();
             timer.Interval = TimeSpan.FromSeconds(10).TotalMilliseconds;
-            timer.Elapsed += (sender, e) => Math();
+            timer.Elapsed += (sender, e) =>
+            {
+                int num1 = 10;
+                int num2 = 5;
+
+                int sum = num1 + num2;
+                int difference = num1 - num2;
+                int product = num1 * num2;
+                double quotient = (double)num1 / num2;
+
+                Console.WriteLine($"Sum: {sum}");
+                Console.WriteLine($"Difference: {difference}");
+                Console.WriteLine($"Product: {product}");
+                Console.WriteLine($"Quotient: {quotient}");
+            };
             timer.Enabled = true;
 
             Console.WriteLine("Wait for it..");
             Console.ReadLine();
         }
-
-        private static void Math()
-        {
-            int num1 = 10;
-            int num2 = 5;
-
-            int sum = num1 + num2;
-            int difference = num1 - num2;
-            int product = num1 * num2;
-            double quotient = (double)num1 / num2;
-
-            Console.WriteLine($"Sum: {sum}");
-            Console.WriteLine($"Difference: {difference}");
-            Console.WriteLine($"Product: {product}");
-            Console.WriteLine($"Quotient: {quotient}");
-        }
     }
+
 }
 
